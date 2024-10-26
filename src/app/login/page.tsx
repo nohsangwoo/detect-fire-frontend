3849a36e611386104bfaf1d12693ac7e1a76fea4
nohsangwoo <nsgr12@naver.com>
2024-10-27 05:56:05 +0900

    feat: 로그인 화면 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ export default function Login() {
 
     return (
         <div className="w-full h-screen bg-black-1 flex justify-center items-center">
-            <div className="flex justify-center items-center w-[700px] h-[700px] rounded-full bg-transparent border border-celadon relative">
+            <div className="flex justify-center items-center w-[700px] h-[700px] rounded-full bg-transparent border-[3px] border-celadon relative">
                 <div className="absolute top-[-50px] left-1/2 transform -translate-x-1/2 flex justify-center items-center">
                     <div>
                         <svg width="100" height="100" viewBox="0 0 100 100" className="fill-celadon">
@@ -18,13 +18,15 @@ export default function Login() {
                     </div>
                 </div>
                 <div className="text-celadon text-2xl font-bold flex flex-col gap-4">
-                    <input type="text" placeholder="username" className="w-[300px] h-[50px] rounded-full bg-transparent border-[3px] border-celadon px-4 ring-0 focus:ring-0 focus:outline-none" />
-                    <input type="password" placeholder="password" className="w-[300px] h-[50px] rounded-full bg-transparent border-[3px] border-celadon px-4 ring-0 focus:ring-0 focus:outline-none" />
+                    <input type="text" placeholder="email" className="w-[300px] h-[50px] rounded-full bg-transparent border-[2px] border-celadon px-4 ring-0 focus:ring-0 focus:outline-none" />
+                    <input type="password" placeholder="password" className="w-[300px] h-[50px] rounded-full bg-transparent border-[2px] border-celadon px-4 ring-0 focus:ring-0 focus:outline-none" />
                     <div className="w-[300px] h-[50px] rounded-full border-[3px] border-celadon text-black-1 flex justify-center items-center text-white text-xl font-extrabold cursor-pointer">
                         login
                     </div>
+                    <div className="flex">
+                        <div className="text-sm cursor-pointer text-celadon">비밀번호 찾기</div>
+                    </div>
                 </div>
-
             </div>
         </div>
     );
